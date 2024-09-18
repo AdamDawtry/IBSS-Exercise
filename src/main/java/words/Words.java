8996a3b8f248f1fd16ce1bc073c3108e6ac8c5c3
Adam Dawtry <94234160+AdamDawtry@users.noreply.github.com>
2024-09-18 15:00:43 +0100

    Implement initial solution to Words problem

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -1,9 +1,23 @@
 package words;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Words {
     public static List<String> getUniqueWordsFromSentence(String sentence) {
-        throw new UnsupportedOperationException();
+    	// Assuming all test cases verify words with lower case
+    	String lowerS = sentence.toLowerCase();
+    	
+    	// Splits on all non alphabetical characters - will break up contracted words with
+    	// apostrophes, e.g. "Isn't" or any possessive, e.g. "Adam's"; unsure if
+    	// this is desired behaviour for the problem
+    	String[] splitWords = lowerS.split("\\P{Alpha}+");
+    	
+    	// Convert to intermediate HashSet to remove duplicates
+    	HashSet<String> a = new HashSet<String>(List.of(splitWords));
+    	
+    	// Convert back to a List for final return value
+    	List<String> ans = List.of(a.toArray(new String[0]));
+        return ans;
     }
 }
