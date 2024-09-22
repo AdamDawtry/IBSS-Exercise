e4ff64860b9e39c4534e8e4c3627529d49b3b66c
Adam Dawtry <94234160+AdamDawtry@users.noreply.github.com>
2024-09-22 22:57:37 +0100

    Change regex to not break up contracted words and possessive names

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -8,10 +8,8 @@ public class Words {
     	// Assuming all test cases verify words with lower case
     	String lowerS = sentence.toLowerCase();
     	
-    	// Splits on all non alphabetical characters - will break up contracted words with
-    	// apostrophes, e.g. "Isn't" or any possessive, e.g. "Adam's"; unsure if
-    	// this is desired behaviour for the problem
-    	String[] splitWords = lowerS.split("\\P{Alpha}+");
+    	// Split on all sequences of non-alphabetical characters, barring apostrophes
+    	String[] splitWords = lowerS.split("[^a-zA-Z']+");
     	
     	// Convert to intermediate HashSet to remove duplicates
     	HashSet<String> a = new HashSet<String>(List.of(splitWords));
